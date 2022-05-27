2ec5c69b3cf75c6398080db3546db5be3fb058f9
Katie Marcks <katiemarcks7@gmail.com>
2022-05-27 06:29:39 -0600

    Pass meal.js tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  };
+}
+
+function addIngredients(ingredient, array) {
+  if (!array.includes(ingredient)) {
+    array.push(ingredient);
+  }
+  return array;
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price - price / 10;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
