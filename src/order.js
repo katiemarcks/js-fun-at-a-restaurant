12ab8cf26319c2bf6896151dbbf75cbe9662450b
Katie Marcks <katiemarcks7@gmail.com>
2022-05-27 07:17:35 -0600

    Pass 10 tests in order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,35 @@
+function takeOrder(order, orderTypeList) {
+  if (orderTypeList.length < 3) {
+    return orderTypeList.push(order);
+  }
+}
+
+function refundOrder(orderNumber, orderTypeList) {
+  let index = orderTypeList.findIndex((order) => {
+    return order.orderNumber === orderNumber;
+  });
+  return orderTypeList.splice(index, 1);
+}
+
+function listItems(orderTypeList) {
+  let list = "";
+  for (let i = 0; i < orderTypeList.length; i++) {
+    let element = orderTypeList[i];
+    if (
+      orderTypeList.length === 1 ||
+      element === orderTypeList[orderTypeList.length - 1]
+    ) {
+      list += element["item"];
+    } else {
+      list += `${element["item"]}, `;
+    }
+  }
+  return list;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
-}
+};
