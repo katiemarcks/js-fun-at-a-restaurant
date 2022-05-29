856e8b9abef3bae6544725aebc4d281f21a3d097
Katie Marcks <katiemarcks7@gmail.com>
2022-05-29 07:46:18 -0600

    Pass 8 Chef.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,24 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(name, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  checkForFood(foodItem) {
+    let mealType = foodItem["type"];
+    let correctMenu = this.restaurant.menus[mealType];
+
+    if (correctMenu.find((food) => food.name === foodItem.name)) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    }
   }
 }
 
